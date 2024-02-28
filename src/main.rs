b2cdbf43d5f42f0a91d2031700dc2de014d28707
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-28 16:29:42 +0100

    Header:DONE

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-
 use std::fs::File;
 use std::io::Read;
-use::std::str;
+use std::str;
 
-fn main() {
-    let mut file = File::open("src/picture/jw02420001001_gs-fg_2022192192555_cal.fits").unwrap();
-
-    let mut buffer = vec![0; 100];
-    file.read_exact(&mut buffer);
-
-    println!("{:?}", buffer);
+fn main() -> Result<(), std::io::Error> {
+    let mut file = File::open("src/picture/jw02420001001_gs-fg_2022192192555_cal.fits")?;
+    
+    let mut buffer = vec![0; 2880];
+    file.read_exact(&mut buffer)?;
+    
+    let header_parts: Vec<&[u8]> = buffer.chunks(80).collect();
+    
+    for header_part in header_parts {
+        println!("> {:?}", str::from_utf8(header_part).unwrap());
+    }
+    
+    Ok(())
 }
