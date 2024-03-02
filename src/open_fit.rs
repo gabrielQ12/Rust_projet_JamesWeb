2b2774cca2c02e184c2bfa55947bf982e930bfb3
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-03-02 16:32:55 +0100

    en tete traiter

--- src/open_fit.rs
+++ src/open_fit.rs
@@ -3,86 +3,79 @@ use std::io::Read;
 use std::str;
 use std::collections::btree_map::BTreeMap;
 
-
+pub struct Hud {
+    header: BTreeMap<String, String>,
+    data: Vec<f32>,
+    
+}
 
 pub fn open_fits(path: &str) -> Result<(), std::io::Error> 
 
 {
     let mut file = File::open(path)?;
     
-    read_hud(&mut file);
+    let _hud1 = read_hud(&mut file);
+    let hud2 = read_hud(&mut file);
+    
+    
+    for (key, value) in hud2.header.iter()
+    {
+        println!("{:?}=> {:?}", key, value);
+    }
     
     Ok (())
 }
 
 
 
-fn read_hud(file: &mut File)
-{
-    let header = read_header(file);
+fn read_hud(file: &mut File) -> Hud {
+    let hud = Hud {
+        header : read_header(file),
+        data : Vec::new(),
+    };
     
-    println!("{:?}", header.len());
     
-    for (key, value) in header.iter()
-    {
-        println!("{}: {}", key, value);
-    }
+    
+    
+    return hud;
+    
 }
 
 fn read_header(file: &mut File) -> BTreeMap<String, String> {
     let mut stop_reading = false;
     let mut header: BTreeMap<String, String> = BTreeMap::new();
     
-    while stop_reading == false {
-        println!("New block");
-        
-        let block = match read_block(file) {
-            Ok(block) => block,
-            Err(_error) => return header,
-        };
-        
-        let header_parts = block.chunks(80);
-        
-        for header_part in header_parts {
-            let row = str::from_utf8(header_part).unwrap().trim();
+    while stop_reading == false 
+    {
+        if let Ok(block) =  read_block(file)
+        {
+            let header_parts = block.chunks(80);
             
-            if row == "END" {
-                stop_reading = true;
-                break;
-            }
-            
-            
-            if row.find("= ") == Some(8) 
+            for header_part in header_parts 
             {
+                let row = str::from_utf8(header_part).unwrap().trim();
                 
-                if let Some((key, right_part)) = row.split_once("= ") {
-                    let mut value = right_part.trim();
-                    if let Some((v, _comment)) = right_part.split_once("/") 
-                    {
-                        
-                        
-                        value = v.trim();
-                    }
-                    
-                    header.insert(key.trim().to_string(), value.trim().to_string());
-                } else {
-                    continue;
+                if row == "END" {
+                    stop_reading = true; 
                 }
-            } else {
-                continue;
-            }
-            
-            let parts = row.split_once("=");
-            
-            if parts.is_none() {
-                continue;
-            }
+                
+                else 
+                
+                {
+                    if let Some ((key, value)) = extract_key_value(row)
+                    {
+                        header.insert(key, value);
+                    }
+                }
+            } 
         }
+        else   
+        {
+            stop_reading = true;
+        };
     }
-    
-    header
-}
-
+    return header
+}    
 
 
 fn read_block(file: &mut File) -> Result<Vec<u8>, std::io::Error>
@@ -94,3 +87,26 @@ fn read_block(file: &mut File) -> Result<Vec<u8>, std::io::Error>
     Ok(buffer)
     
 }
+
+
+fn extract_key_value(row: &str) -> Option<(String, String)> 
+{
+    if row.find("= ") == Some(8) 
+    {
+        
+        let (key, right_part) = row.split_once("= ")?;
+        let mut value = right_part;
+        if let Some((v, _comment)) = right_part.split_once("/") 
+        {
+            
+            
+            value = v;
+        }
+        return Some((String::from(key.trim()), String::from(value.trim())))
+        
+    }
+    return None;
+}
+
+
+
